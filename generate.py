729fd95c61c9b359f488bad346725513feba71cf
Nick S. Yu <yunicks@leagueofevil.ab.ca>
2020-10-05 14:56:03 -0600

    hack hack hack.  Programming is hard work

--- generate.py
+++ generate.py
@@ -8,7 +8,11 @@ import json
 import os
 import sys
 
-CREDENTIAL_FILE_PATH = "./.credentials"
+CREDENTIAL_FILE_PATH = "./.credential"
+
+def decode_flag(credential):
+    """ Decode the flag, given the given credential. """
+    return "TODO: finish this!"
 
 def read_credential_file():
     """ Reads the contents of the current directory's .credential
@@ -18,8 +22,6 @@ def read_credential_file():
             credential = f.read()
             if len(credential) != 23:
                 raise Exception("Invalid credential file.")
-
-            print("Valid credential file found!")
             return credential
     except IOError as e:
         raise Exception("No credential file found.  Contact Big Boss for one.")
@@ -28,6 +30,10 @@ def main():
     """ The entry point to the script."""
     print(" --- welcome to generate.py ---")
     credential = read_credential_file()
+
+    print("Valid credential found!")
+    print("The flag is: \"{}\".  Goodbye!".format(decode_flag(credential)))
+
     return 0
 
 if __name__ == "__main__":
