6bb692e6e02b80956b3f383b0164d3ecf37b19e4
Nick S. Yu <yunicks@leagueofevil.ab.ca>
2020-10-05 15:12:36 -0600

    Move cypher into its own dedicated file at .cypher
    This mimics how the secret credential file should be stored, too.
    
    wip

--- generate.py
+++ generate.py
@@ -8,8 +8,8 @@ import json
 import os
 import sys
 
+CYPHER_FILE_PATH = "./.cypher"
 CREDENTIAL_FILE_PATH = "./.credential"
-CYPHER = 'bzssppgbtctgemlsxgiagdl'
 
 def decode_char(x,y):
     """ Sums two characters' according to their place in the
@@ -18,33 +18,46 @@ def decode_char(x,y):
     y = ord(y) - ord('a')
     return chr(ord('a') + ((x + y) % 26))
 
-def decode_flag(credential):
+def decode_flag(credential, cypher):
     """ Decode the flag, given the given credential.  Applies
     decode_char() to every letter in the cypher and the credential."""
     ret = []
     for i in range(len(credential)):
-        ret.append(decode_char(credential[i], CYPHER[i]))
+        ret.append(decode_char(credential[i], cypher[i]))
     return "".join(ret)
 
 def read_credential_file():
     """ Reads the contents of the current directory's .credential
     file."""
-    try:
-        with open(CREDENTIAL_FILE_PATH) as f:
-            credential = f.read().strip()
-            if len(credential) != 23:
-                raise Exception("Invalid credential file.")
-            return credential
-    except IOError as e:
-        raise Exception("No credential file found.  Contact Big Boss for one.")
+    with open(CREDENTIAL_FILE_PATH) as f:
+        credential = f.read().strip()
+        if len(credential) != 23:
+            raise Exception("Invalid credential file.")
+        print("Valid credential at {} found!".format(CREDENTIAL_FILE_PATH))
+        return credential
+
+def read_cypher_file():
+    """ Reads the contents of the current directory's .credential
+    file."""
+    with open(CYPHER_FILE_PATH) as f:
+        credential = f.read().strip()
+        if len(credential) != 23:
+            raise Exception("Invalid cypher file.")
+        print("Valid cypher file at {} found!".format(CYPHER_FILE_PATH))
+        return credential
 
 def main():
     """ The entry point to the script."""
     print(" --- welcome to generate.py ---")
-    credential = read_credential_file()
 
-    print("Valid credential found!")
-    print("The flag is: \"{}\".  Goodbye!".format(decode_flag(credential)))
+    try:
+        cypher = read_cypher_file()
+        credential = read_credential_file()
+    except IOError:
+        print("I don't have all the files I need!  GOODBYE!!!!")
+        return 1
+
+    print("The flag is: \"{}\".  Goodbye!".format(decode_flag(credential, cypher)))
 
     return 0
 
