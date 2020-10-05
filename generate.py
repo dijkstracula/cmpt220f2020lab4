3b2d69797161d259e6604f0b0562b230c2a04272
Nick S. Yu <yunicks@leagueofevil.ab.ca>
2020-10-05 14:44:05 -0600

    Implement credential-reading function
    I'll still need a test credential to test this functionality with.
    Better get one from Big Boss...

--- generate.py
+++ generate.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python
 
+""" generate.py: Prints out the flag, given the credential file.
+    author: Nick S. Yu
+"""
+
+import json
 import os
 import sys
 
-""" generate.py: Prints out the flag, given the config file.
-    author: Nick S. Yu
-"""
+CREDENTIAL_FILE_PATH = "./.credentials"
+
+def read_credential_file():
+    """ Reads the contents of the current directory's .credential
+    file."""
+    with open(CONFIG_FILE_PATH) as f:
+        credential = f.read()
+        if len(credential) != 16:
+            raise Exception("Invalid credential file.")
+        return credential
 
 def main():
     """ The entry point to the script."""
